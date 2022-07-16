fbe43fbffa29072657605a1e57ac3716217c63a5
MiniMinja <minyoungheo.heo@gmail.com>
2022-07-16 13:24:53 -0700

    updated scripts

---
+++ Assets/Scripts/Character/Movement/DirectionalMovement.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalMovement : Movement
+{
+    public override void Move(Rigidbody2D rb)
+    {
+        
+    }
+}
