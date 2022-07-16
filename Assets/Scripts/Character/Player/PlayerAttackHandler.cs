066501ebf4dfa9445cc075481091fb996361f9c8
MiniMinja <minyoungheo.heo@gmail.com>
2022-07-16 11:52:15 -0700

    finished merge conflicts part 2

--- Assets/Scripts/Character/Player/PlayerAttackHandler.cs
+++ Assets/Scripts/Character/Player/PlayerAttackHandler.cs
@@ -4,20 +4,12 @@ using UnityEngine;
 
 public class PlayerAttackHandler : MonoBehaviour
 {
-<<<<<<< HEAD
     public CharacterData charData;
 
     WeaponHitboxActivator activator;
     string[] weaponChoices;
     int currWeaponIndex;
     bool pauseRotate;
-=======
-    public Camera cam;
-
-    public CharacterData charData;
-
-    WeaponHitboxActivator activator;
->>>>>>> cfff4a0bd7d9e654b18b9749ce31b0c6ece18420
 
     bool timeout;
     float timeduration;
@@ -25,11 +17,8 @@ public class PlayerAttackHandler : MonoBehaviour
     void Start()
     {
         activator = GetComponentInChildren<WeaponHitboxActivator>();
-<<<<<<< HEAD
         currWeaponIndex = 0;
         weaponChoices = new string[]{"sword", "gun"};
-=======
->>>>>>> cfff4a0bd7d9e654b18b9749ce31b0c6ece18420
 
         timeout = false;
         timeduration = 0;
@@ -47,17 +36,11 @@ public class PlayerAttackHandler : MonoBehaviour
         {
             if(!timeout)
             {
-<<<<<<< HEAD
                 activator.Activate();
-=======
-                Vector2 dir = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-                activator.Activate(Mathf.Atan2(dir.y, dir.x) / Mathf.PI * 180f);
->>>>>>> cfff4a0bd7d9e654b18b9749ce31b0c6ece18420
                 timeout = true;
                 timeduration = 0;
             }
         }
-<<<<<<< HEAD
 
         if(Input.GetKeyDown(KeyCode.P))
         {
@@ -82,7 +65,5 @@ public class PlayerAttackHandler : MonoBehaviour
         }
         activator.SetWeapon(weaponChoices[currWeaponIndex]);
         Debug.Log("Current Weapon: " + weaponChoices[currWeaponIndex]);
-=======
->>>>>>> cfff4a0bd7d9e654b18b9749ce31b0c6ece18420
     }
 }
